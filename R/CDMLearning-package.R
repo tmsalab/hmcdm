507bc6ecf222e0ab1f70dc6d84fc782ce168ef01
Susu Zhang <susu.zhang1992@gmail.com>
2018-04-05 22:33:04 -0500

    Added references.

---
+++ R/CDMLearning-package.R
@@ -0,0 +1,6 @@
+#' @useDynLib CDMLearning, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+#' @references 
+#' Wang, S., Yang, Y., Culpepper, S. A., & Douglas, J. A. (2018). Tracking Skill Acquisition With Cognitive Diagnosis Models: A Higher-Order, Hidden Markov Model With Covariates. Journal of Educational and Behavioral Statistics, 1076998617719727.
+#' Chen, Y., Culpepper, S. A., Wang, S., & Douglas, J. (2018). A hidden Markov model for learning trajectories in cognitive diagnosis with application to spatial rotation skills. Applied Psychological Measurement, 42(1), 5-23.
+"_PACKAGE"
