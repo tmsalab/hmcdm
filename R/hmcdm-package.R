e293ea79ac95031a98cca8cddc7a7c956d78c9bd
Susu Zhang <susu.zhang1992@gmail.com>
2018-04-07 17:49:33 -0500

    Change package name to `hmcdm`.

--- R/hmcdm-package.R
+++ R/hmcdm-package.R
@@ -1,6 +1,9 @@
-#' @useDynLib CDMLearning, .registration = TRUE
+#' @useDynLib hmcdm, .registration = TRUE
 #' @importFrom Rcpp evalCpp
 #' @references 
 #' Wang, S., Yang, Y., Culpepper, S. A., & Douglas, J. A. (2018). Tracking Skill Acquisition With Cognitive Diagnosis Models: A Higher-Order, Hidden Markov Model With Covariates. Journal of Educational and Behavioral Statistics, 1076998617719727.
+#' 
 #' Chen, Y., Culpepper, S. A., Wang, S., & Douglas, J. (2018). A hidden Markov model for learning trajectories in cognitive diagnosis with application to spatial rotation skills. Applied Psychological Measurement, 42(1), 5-23.
+#' 
+#' Wang, S., Zhang, S., Douglas, J., & Culpepper, S. (2018). Using Response Times to Assess Learning Progress: A Joint Model for Responses and Response Times. Measurement: Interdisciplinary Research and Perspectives, 16(1), 45-58.
 "_PACKAGE"
